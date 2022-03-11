c3ee9a3bc816438665aa9cf1258c855dde8e285c
wgreenberger <99290925+wgreenberger@users.noreply.github.com>
2022-03-11 13:27:59 -0500

    Add files via upload
    Still experiencing issues with the drop down box instead of text box

--- Problem with output GroupAssignment2.R
+++ Problem with output GroupAssignment2.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinydashboard)
 library(semantic.dashboard)
 library(ggplot2)
 library(plotly)
@@ -7,11 +8,9 @@ library(fpp3)
 library(ggpubr)
 library(plotly)
 
-
+library(tidyverse)
 library(quantmod)
 library(tidyquant)
-
-
 
 
 
@@ -51,20 +50,23 @@ ui <- dashboardPage(
       tabItem(
         tabName = "main",
         fluidPage(
-          textInput("text", label = h3("Select a Stock"), value = "Enter Stock Symbol"),
           
-          hr(),
-          fluidRow(column(3, verbatimTextOutput("value")))
+          
+          selectInput(inputId = "select", label = h3("Choose a Stock"), 
+                      choices = names(table(SYMBOLS$Name)), 
+                      selected = 1)
+          
+          
           
         )
         
       )
     )
-  ), theme = "cerulean"
+  )
 )
 
 
-server <- shinyServer(function(input, output, session) {
+server <- shinyServer(function(input, output) {
   start <- as.Date("2016-01-01")
   end <- as.Date("2022-01-01")
   interest_stocks <- c("AAPL", "MSFT", "GOOG", "AMZN")
@@ -73,12 +75,16 @@ server <- shinyServer(function(input, output, session) {
     ggplotly(ggplot(Stocks, aes(date, close)) + geom_point(aes(date, close, color = symbol)))
   })
   
-  interest_stocks2 <- c(observe(as.character(input$text)))
-  Stocks2 <- tq_get(interest_stocks2, from = start, to = end)
-  output$dotplot1 <- renderPlotly({
+  output$value <- renderPrint({SYMBOLS$Symbol[which(SYMBOLS$Name == input$select)]})
+  
+  interest_stocks2 <- getSymbols({SYMBOLS$Symbol[which(SYMBOLS$Name == input$select)]})
+  Stocks2 <- tq_get(reactive(interest_stocks2, from = start, to = end))
+  #Stocks2 <- Symbols21 %>% filter(symbol %in% input$select)
+  
+  
+   output$dotplot1 <- renderPlotly({
     ggplotly(ggplot(Stocks2, aes(date, close)) + geom_point(aes(date, close, color = symbol)))
   })
-  output$value <- renderPrint({ input$text })
   
   
 })
