10e2fa25f7b7c1b75f51a9af2d472d7cd6b86f98
wgreenberger <99290925+wgreenberger@users.noreply.github.com>
2022-03-10 21:31:40 -0500

    Add files via upload
    Added a dashboard feature as well as a plotly for multiple big stocks for comparison to chosen stock. We plan to add a text box and to allow the user to select their own stock and compare performance over the past six years.

---
+++ GroupAssignment2.R
@@ -0,0 +1,82 @@
+library(shiny)
+library(semantic.dashboard)
+library(ggplot2)
+library(plotly)
+library(DT)
+library(fpp3)
+library(ggpubr)
+library(plotly)
+
+
+library(quantmod)
+library(tidyquant)
+start <- as.Date("2016-01-01")
+end <- as.Date("2022-01-01")
+interest_stocks <- c("AAPL", "MSFT", "GOOG", "AMD")
+Stocks <- tq_get(interest_stocks, from = start, to = end)
+
+
+
+
+SYMBOLS <- stockSymbols()
+
+ui <- dashboardPage(
+  dashboardHeader(color = "blue",title = "Stock Price Data", inverted = TRUE),
+  dashboardSidebar(
+    size = "thin", color = "teal",
+    sidebarMenu(
+      menuItem(tabName = "main", "Select a Stock"),
+      menuItem(tabName = "extra", "Your Stock Performance Compared to Major Performances")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      selected = 1,
+      tabItem(
+        tabName = "extra",
+        fluidRow(
+          box(width = 8,
+              title = "4 Major Companies",
+              color = "green", ribbon = TRUE, title_side = "top right",
+              column(width = 8,
+                     plotlyOutput("dotplot")
+              )
+          ),
+          box(width = 8,
+              title = "Graph 2",
+              color = "red", ribbon = TRUE, title_side = "top right",
+              column(width = 8,
+                     plotlyOutput("dotplot1")
+              )
+          )
+        )
+      ),
+      tabItem(
+        tabName = "main",
+        fluidRow(
+          dataTableOutput("carstable")
+        )
+      )
+    )
+  ), theme = "cerulean"
+)
+
+server <- shinyServer(function(input, output, session) {
+  start <- as.Date("2016-01-01")
+  end <- as.Date("2022-01-01")
+  interest_stocks <- c("AAPL", "MSFT", "GOOG", "AMZN")
+  Stocks <- tq_get(interest_stocks, from = start, to = end)
+  output$dotplot <- renderPlotly({
+    ggplotly(ggplot(Stocks, aes(Date, Closing Dollar)) + geom_point(aes(date, close, color = symbol)))
+  })
+  
+  output$dotplot1 <- renderPlotly({
+    ggplotly(ggplot(mtcars, aes(wt, mpg))
+             + geom_point(aes(colour=factor(cyl), size = qsec))
+             + scale_colour_manual(values = colscale)
+    )
+  })
+  output$carstable <- renderDataTable(mtcars)
+})
+
+shinyApp(ui, server)
