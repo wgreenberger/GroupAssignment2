de2c19b27c79a27169401a4ecc564f29dc0eb8d0
wgreenberger <99290925+wgreenberger@users.noreply.github.com>
2022-03-11 19:44:39 -0500

    Add files via upload
    Final attempt for shiny app

---
+++ Project2 final attempt.R
@@ -0,0 +1,108 @@
+library(shiny)
+library(shinydashboard)
+library(semantic.dashboard)
+library(ggplot2)
+library(plotly)
+library(DT)
+library(fpp3)
+library(ggpubr)
+library(plotly)
+
+library(tidyverse)
+library(quantmod)
+library(tidyquant)
+
+
+
+start <- as.Date("2016-01-01")
+end <- as.Date("2022-01-01")
+Stocks <- tq_get(c("AAPL", "MSFT", "GOOG", "AMZN"), from = start, to = end)
+
+
+
+SYMBOLS <- stockSymbols()
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Stock Price Data", inverted = TRUE),
+  dashboardSidebar(
+    size = "thin", color = "teal",
+    sidebarMenu(
+      menuItem(tabName = "main", "Select a Stock"),
+      menuItem(tabName = "extra", "Your Stock Performance Compared to Major Performances")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      selected = 1,
+      tabItem(
+        tabName = "extra",
+        fluidRow(
+          box(width = 8,
+              title = "4 Major Companies",
+              color = "green", ribbon = TRUE, title_side = "top right",
+              column(width = 8,
+                     plotlyOutput("dotplot")
+              )
+          ),
+          box(width = 8,
+              title = "Your Chosen Stock",
+              color = "red", ribbon = TRUE, title_side = "top right",
+              column(width = 8,
+                     plotlyOutput("dotplot1")
+              )
+          )
+        )
+      ),
+      tabItem(
+        tabName = "main",
+        fluidPage(
+          
+          
+          selectInput(inputId = "select", label = h3("Choose a Stock"), 
+                      choices = names(table(SYMBOLS$Name)), 
+                      selected = 1)
+          
+          
+          
+        )
+        
+      )
+    )
+  )
+)
+
+
+server <- shinyServer(function(input, output) {
+  
+  
+  #Selected_stock<- reactive({
+#names(table(SYMBOLS$Name))[input$select]
+ # })
+  
+  #stock_ticker <- reactive({
+  
+  #SYMBOLS$Symbol[which(SYMBOLS$Name == Selected_stock)]
+  #})
+  
+  
+  Stocks2 <- reactive({
+    Stocks21 <- as.tibble(tq_get((SYMBOLS$Symbol[which(SYMBOLS$Name ==  names(table(SYMBOLS$Name))[input$select])]), from = start, to = end))
+    print(Stocks21)
+    Stocks21
+  })
+  
+
+  output$dotplot <- renderPlotly({
+    ggplotly(ggplot(Stocks, aes(date, close)) + geom_point(aes(date, close, color = symbol)))
+  })
+  
+  output$value <- renderPrint({SYMBOLS$Symbol[which(SYMBOLS$Name == input$select)]})
+  
+
+  output$dotplot1 <- renderPlotly({
+    ggplotly(ggplot(Stocks2(), aes_string(x = date, y = close)) + geom_point(aes_string(date, close, color = symbol)))
+  })
+  
+})
+shinyApp(ui, server)
+
